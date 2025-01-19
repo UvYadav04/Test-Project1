cee1991e2b0239d4605fd182fcec6e0ccbfbf7c2
Dinesh Yadav <dineshnirban01@gmail.com>
2025-01-19 13:14:04 +0530

    modified card2

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { GoDotFill } from "react-icons/go";
 
 import React from 'react'
 function Card2({ check, selected, status, credits, amount, id }) {
@@ -8,7 +9,7 @@ function Card2({ check, selected, status, credits, amount, id }) {
             <tr className='py-4 border-[1px] flex justify-evenly max-w-[100vw] overflow-hidden'>
                 <td className=' w-16 text-center text-xl'><input onChange={() => check(id)} defaultChecked={selected} checked={selected} type="checkbox" /></td>
                 <td className='md:w-32 flex-1 text-start'>{id}</td>
-                <td className='md:w-32 flex-1 text-start text-green-700'>{status === "ok" ? "Completed" : "Incomplete"}</td>
+                <td className='md:w-32 flex-1 text-start text-green-700'>{status === "ok" ? <><GoDotFill className="inline mb-1" color={'rgb(21 128 61)'} /> Complete </> : "Incomplete"}</td>
                 <td className='md:w-32 flex-1 text-start'>{credits}</td>
                 <td className='md:w-32 flex-1 text-start'>{amount}</td>
             </tr>
