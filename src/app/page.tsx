0c04bfb7b5f4aca119fc5ff1346b0eeb363cdbda
Dinesh Yadav <dineshnirban01@gmail.com>
2025-01-19 12:55:42 +0530

    move homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@
 import Image from "next/image";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Sidebar from "@/Components/Sidebar";
+import RightPage from "@/Components/ContentPage";
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/Home')
-  }, [])
   return (
     <>
-      Loading
+      <div className='flex flex-row w-full relative'>
+        <Sidebar />
+        <RightPage />
+      </div>
     </>
   );
 }
